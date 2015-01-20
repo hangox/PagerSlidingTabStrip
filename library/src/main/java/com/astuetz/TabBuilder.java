3ffc94aded6c85817e7c0ff170ee81b30feab073
47 <liang.hanguang93@gmail.com>
2015-01-20 13:43:26 +0800

    修改了默认参数

---
+++ library/src/main/java/com/astuetz/TabBuilder.java
@@ -0,0 +1,13 @@
+package com.astuetz;
+
+import android.view.View;
+
+/**
+ * Created With Android Studio
+ * User 4
+ * Date 2015/1/9
+ * Time 9:58
+ */
+public interface TabBuilder {
+    public View createTab();
+}
